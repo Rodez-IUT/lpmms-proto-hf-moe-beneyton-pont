812667a2f38fc31e3e67d991660615ce11e4dca8
charlotte46 <charlotte.pont@iut-rodez.fr>
2020-04-07 08:27:31 +0200

    vue authentification : fini

--- app/src/main/java/com/example/trainingplan/MainActivity.kt
+++ app/src/main/java/com/example/trainingplan/MainActivity.kt
@@ -1,27 +1,31 @@
 package com.example.trainingplan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var souvenir:RadioButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        souvenir = findViewById(R.id.rb_souvenir)
     }
 
-    /**
-     * Permet de cocher ou de décocher le bouton
-     * " se souvenir de moi "
-     */
-    fun decocher(view: View) {
+    fun connexion_fbk(view:View) {
+        // bouchon
+    }
 
+    fun connexion_twitter(view: View) {
+        // bouchon
+    }
+
+    fun valider(view: View) {
+        // vérifier que le format de l'adresse est bien valide
+        // vérifier que les deux champs ont bien été saisi
+        // mettre les deux champs dans des préférences si se souvenir de moi a été sélectionné
+        var intention = Intent(this, FormActivity::class.java)
+        startActivity(intention)
     }
 
 }
